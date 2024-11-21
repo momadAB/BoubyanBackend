acce1181d9cbdf674625f76d5985610948c41ec3
momadAB <mabaqer99@gmail.com>
2024-11-21 08:41:27 +0300

    Init

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-//    List<UserEntity> findByStatus(Status status);
     // Find by username, ignoring case
     Optional<UserEntity> findByUsernameIgnoreCase(String username);
     boolean existsByUsernameIgnoreCase(String username);
